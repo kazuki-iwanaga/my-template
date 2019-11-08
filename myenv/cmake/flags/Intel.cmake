f430cecace08b9bf51bbc5069f65f37d23a884df
kazuki-iwanaga <iwanaga.kazuki.sj@alumni.tsukuba.ac.jp>
2019-11-08 21:16:52 +0900

    delete myenv

--- myenv/cmake/flags/Intel.cmake
+++
@@ -1,9 +0,0 @@
-# https://software.intel.com/en-us/node/677967
-
-set(WARN_FLAGS "-warn all")
-set(NOWARN_FLAGS "-warn none")
-set(PP_FLAGS "-fpp")
-set(OPENMP_FLAGS "-fopenmp")
-
-set(DEBUG_FLAGS "-g -traceback -check all")
-set(RELEASE_FLAGS "-O3")
